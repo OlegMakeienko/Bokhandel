ac128b3058398f665e3532197725968d920f9597
olegmakeienko <molva83@gmail.com>
2024-05-16 23:42:59 +0200

    admin and roles removed from project

---
+++ Bokhandel/Migrations/20240516214117_removeAdmin&roles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bokhandel.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeAdminroles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
